87e37bc6bbf0da729efa1a94a1ebcdb927a89283
Ferlynn-potatoes <s10273710@connect.np.edu.sg>
2026-02-06 23:00:48 +0800

    create dummy html

--- JS/Auth.js
+++ JS/Auth.js
@@ -1,3 +1,17 @@
 // assets/js/auth.js
 if(!store.get(KEYS.role)) store.set(KEYS.role,'guest');
 ``
+//click social icons
+document.querySelector('.social-icons a').forEach(icon => {
+    icon.addEventListerner('click', function(e)
+{
+    e.preventDefault();
+    window.open(this.href, '_blank');
+})})
+
+document.querySelectorAll('.top-bar .contact span').forEach(item => {
+    item.style.cursor = 'pointer';
+    item.addEventListener('click', () => {
+        window.location.href = 'Contact.html';
+    })
+})
