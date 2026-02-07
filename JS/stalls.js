257b138d817f3b4c9920b811495fafc68b741f1c
sssheli <s10272432@connect.np.edu.sg>
2026-02-07 22:24:42 +0800

    edited stalls

--- JS/stalls.js
+++ JS/stalls.js
@@ -2,25 +2,34 @@
 
   const stallsEl = document.getElementById('stallList');
 
+  const LOCAL_STALLS = [
+    { id: 'gw', name: 'Golden Wok', cuisine: 'Chinese Cuisine', hours: '7:00am - 3:00pm', rating: 4.6 },
+    { id: 'mcd', name: 'Mak Cik Delights', cuisine: 'Malay Cuisine', hours: '10:00am - 9:00pm', rating: 4.4 },
+    { id: 'lie', name: 'Little India Express', cuisine: 'Indian Cuisine', hours: '8:00am - 9:00pm', rating: 4.3 },
+    { id: 'tyh', name: 'Tom Yum House', cuisine: 'Thai Cuisine', hours: '11:00am - 10:00pm', rating: 4.5 },
+    { id: 'wb', name: 'Western Bites', cuisine: 'Western Cuisine', hours: '8:00am - 10:00pm', rating: 4.2 },
+    { id: 'ktc', name: 'Kopi & Teh Corner', cuisine: 'Beverages', hours: '8:00am - 10:00pm', rating: 4.7 }
+  ];
+
   function loadStalls() {
 
     stallsEl.innerHTML = '';
 
-    STALLS.forEach(s => {
+    LOCAL_STALLS.forEach(s => {
 
       const card = document.createElement('div');
       card.className = 'stall-card';
 
       card.innerHTML = `
         <div class="stall-top">
-          <h3>${s.cuisine}</h3>
+          <h3>${s.name}</h3>
           <span class="stall-hours">
             Operating hours: ${s.hours}
           </span>
         </div>
 
         <div class="stall-desc">
-          ${s.name} specialises in ${s.cuisine} cuisine. 
+          ${s.name} specialises in ${s.cuisine}. 
           Rated ⭐ ${s.rating}.
         </div>
 
