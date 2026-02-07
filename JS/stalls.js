4071d29dff707d0d9de12c6d42483b0fca1a1995
sssheli <s10272432@connect.np.edu.sg>
2026-02-07 22:59:18 +0800

    edited stalls js

--- JS/stalls.js
+++ JS/stalls.js
@@ -3,13 +3,14 @@
   const stallsEl = document.getElementById('stallList');
 
   const LOCAL_STALLS = [
-    { id: 'gw', name: 'Golden Wok', cuisine: 'Chinese Cuisine', hours: '7:00am - 3:00pm', rating: 4.6 },
-    { id: 'mcd', name: 'Mak Cik Delights', cuisine: 'Malay Cuisine', hours: '10:00am - 9:00pm', rating: 4.4 },
-    { id: 'lie', name: 'Little India Express', cuisine: 'Indian Cuisine', hours: '8:00am - 9:00pm', rating: 4.3 },
-    { id: 'tyh', name: 'Tom Yum House', cuisine: 'Thai Cuisine', hours: '11:00am - 10:00pm', rating: 4.5 },
-    { id: 'wb', name: 'Western Bites', cuisine: 'Western Cuisine', hours: '8:00am - 10:00pm', rating: 4.2 },
-    { id: 'ktc', name: 'Kopi & Teh Corner', cuisine: 'Beverages', hours: '8:00am - 10:00pm', rating: 4.7 }
-  ];
+  { id: 'golden-wok', name: 'Golden Wok', cuisine: 'Chinese Cuisine', hours: '10:00am - 8:00pm', rating: 4.6 },
+  { id: 'mak-cik-delights', name: 'Mak Cik Delights', cuisine: 'Malay Cuisine', hours: '9:00am - 7:00pm', rating: 4.4 },
+  { id: 'little-india-express', name: 'Little India Express', cuisine: 'Indian Cuisine', hours: '8:00am - 9:00pm', rating: 4.3 },
+  { id: 'tom-yum-house', name: 'Tom Yum House', cuisine: 'Thai Cuisine', hours: '11:00am - 10:00pm', rating: 4.5 },
+  { id: 'western-bites', name: 'Western Bites', cuisine: 'Western Cuisine', hours: '10:00am - 9:00pm', rating: 4.2 },
+  { id: 'kopi-teh-corner', name: 'Kopi & Teh Corner', cuisine: 'Beverages', hours: '8:00am - 10:00pm', rating: 4.7 }
+];
+
 
   function loadStalls() {
 
