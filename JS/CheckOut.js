5be41b8cb72c99639d1a1a251be7dda92a7d8719
Ferlynn-potatoes <s10273710@connect.np.edu.sg>
2026-02-08 12:58:26 +0800

    Edited removebtn

--- JS/CheckOut.js
+++ JS/CheckOut.js
@@ -71,7 +71,12 @@ cartDiv.innerHTML = "";
       const removeBtn = itemDiv.querySelector(".delete-btn");
       removeBtn.addEventListener("click", function(){
         const currentCart = getCart();
-        currentCart.splice(index, 1);
+        const item = currentCart[index];
+        if (item.qty > 1) {
+          item.qty -= 1;  //minus 1
+        } else {
+          currentCart.splice(index, 1); //remove if only 1 item
+        }
         setCart(currentCart);
         displayCheckout();
       })
